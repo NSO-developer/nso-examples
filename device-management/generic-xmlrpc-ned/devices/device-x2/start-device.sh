9df1bf2f49928d82b38d7710d3c3068b1f08b90f
Conny Ohult Andersen <cohult@cisco.com>
2026-02-09 11:22:59 +0100

    NSO 6.6.3

--- device-management/generic-xmlrpc-ned/devices/device-x2/start-device.sh
+++ device-management/generic-xmlrpc-ned/devices/device-x2/start-device.sh
@@ -10,7 +10,7 @@ DEVICE_MODEL=../device-common/build/jar/device-model.jar
 XMLRPC_SERVER=../../packages/common/shared-jar/xmlrpc-server-3.1.3.jar
 XMLRPC_COMMON=../../packages/common/shared-jar/xmlrpc-common-3.1.3.jar
 XMLRPC_CLIENT=../../packages/common/shared-jar/xmlrpc-client-3.1.3.jar
-COMMON_LOGGING=../../packages/common/shared-jar/commons-logging-1.1.jar
+COMMON_LOGGING=../../packages/common/shared-jar/commons-logging-1.3.5.jar
 WS_COMMONS_UTIL=../../packages/common/shared-jar/ws-commons-util-1.0.2.jar
 
 CLASSPATH=.:${LOG4JAPI}:${LOG4JCORE}:${JUNIXSOCKC}:${JUNIXSOCKNC}:build/classes:${XMLRPC_SERVER}:${XMLRPC_COMMON}:${XMLRPC_CLIENT}:${COMMON_LOGGING}:${WS_COMMONS_UTIL}:${DEVICE_JAR}:${DEVICE_MODEL}
