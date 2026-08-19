2721ddc0af371e23b60f7e3c21ce24edf46dc636
cohult <cohult@cisco.com>
2026-08-19 03:53:52 +0200

    NSO 6.5.11
    Signed-off-by: cohult <cohult@cisco.com>

--- common/packages/dell-ftos-netsim-cli-1.0/src/java/src/com/tailf/packages/ned/netsimcli/namespaces/netsimNedDellFtosStats.java
+++ common/packages/dell-ftos-netsim-cli-1.0/src/java/src/com/tailf/packages/ned/netsimcli/namespaces/netsimNedDellFtosStats.java
@@ -62,10 +62,10 @@ public class netsimNedDellFtosStats extends ConfNamespace {
     public static final String _underruns_ = "underruns";
     public static final int    _state = 630973766;
     public static final String _state_ = "state";
-    public static final int    _pkts_over_255_byte = 1831081435;
-    public static final String _pkts_over_255_byte_ = "pkts-over-255-byte";
     public static final int    _pkts_over_511_byte = 542466306;
     public static final String _pkts_over_511_byte_ = "pkts-over-511-byte";
+    public static final int    _pkts_over_255_byte = 1831081435;
+    public static final String _pkts_over_255_byte_ = "pkts-over-255-byte";
     public static final int    _runts = 1233238779;
     public static final String _runts_ = "runts";
     public static final int    _arp_type = 463658930;
@@ -104,10 +104,10 @@ public class netsimNedDellFtosStats extends ConfNamespace {
     public static final String _packets_p_sec_ = "packets-p-sec";
     public static final int    _giants = 818355955;
     public static final String _giants_ = "giants";
-    public static final int    _last_clearing_counter = 2004021148;
-    public static final String _last_clearing_counter_ = "last-clearing-counter";
     public static final int    _discarded = 1973722861;
     public static final String _discarded_ = "discarded";
+    public static final int    _last_clearing_counter = 2004021148;
+    public static final String _last_clearing_counter_ = "last-clearing-counter";
     public static final int    _unicasts = 766465200;
     public static final String _unicasts_ = "unicasts";
     public static final int    _multicasts = 776393491;
